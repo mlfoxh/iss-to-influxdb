97b41ce07d47244bb4252a21b59ff5c9a0656923
mlfh <matthew@foxhumphreys.com>
2020-10-21 16:12:39 -0700

    changed field name to time_epoch

--- iss-to-influxdb.sh
+++ iss-to-influxdb.sh
@@ -33,7 +33,7 @@ do
 	# Send values to InfluxDB
 	echo "Sending values to InfluxDB..."
 	curl -v --output /dev/null -i -XPOST "$INFLUXDB_ADDRESS/write?db=$INFLUXDB_DATABASE&u=$INFLUXDB_USER&p=$INFLUXDB_PASSWORD" --data-binary \
-		"position latitude=$LAT,longitude=$LONG,time=$TIME $TIME_NANO"
+		"position latitude=$LAT,longitude=$LONG,time_epoch=$TIME $TIME_NANO"
 
 	# Repeat after interval (seconds)
 	echo "Sleeping for $INTERVAL seconds..."
